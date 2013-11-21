bc0a1e5243b4718946401ab9a20ff27a23dba104
Jeff Sheltren <jeff@tag1consulting.com>
2013-11-21 09:45:50 -0800

    What seems to be a working LAMP stack

--- Puppetfile
+++ Puppetfile
@@ -3,8 +3,8 @@ forge 'forge.puppetlabs.com'
 
 # Install modules from the Forge
 mod 'puppetlabs/apache', '0.9.0'
+mod 'puppetlabs/concat', '1.0.0'
 mod 'puppetlabs/mysql', '2.1.0'
 mod 'puppetlabs/stdlib', '3.2.0'
 mod 'puppetlabs/vcsrepo', '0.1.1'
 mod 'stahnma/epel', '0.0.5'
-mod 'thias/php', '0.3.7'
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,8 @@
 node default {
   stage { 'pre': before => Stage['main'] }
   class { 'epel': stage => 'pre' }
-  class { 'common': }
 
   class { 'apache': }
-  class { 'mysql': }
+  class { 'apache::mod::php': }
   class { 'mysql::server': }
-  class { 'mysql::java': }
-  class { 'mysql::python': }
-  class { 'mysql::ruby': }
-  class { 'php::cli': }
-  class { 'php::mod_php5': }
 }
